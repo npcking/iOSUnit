b12ef65278725ffb436be67b2cb093ef555b3647
Thanh Dung TRUONG <braden.truong@outlook.com>
2013-08-27 13:57:26 +1000

    add assignment01

---
+++ assignment01/CrashAndBurn/CrashAndBurn/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  CrashAndBurn
+//
+//  Created by Mark Makdad on 5/30/11.
+//  Copyright 2011 Long Weekend Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "CrashAndBurnAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+   @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([CrashAndBurnAppDelegate class]));
+   }
+}
